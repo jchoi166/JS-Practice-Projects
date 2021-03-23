52fc9b33b720e5c01442069a9375126cddb7e7db
Spike <jchoi166@gmail.com>
2021-03-23 08:08:37 -0700

    basic functionality working - 1 time only

--- Dollars-To-Cents-App/script.js
+++ Dollars-To-Cents-App/script.js
@@ -3,20 +3,56 @@
 console.log("hello there")
 
 const form = document.querySelector("#converter")
-// const input = document.querySelector("#dollar-input")
+const input = document.querySelector("#dollar-input")
+const coinList = document.querySelectorAll(".coin-list p")
 
-form.addEventListener("submit", (event)=> {
+let centsArray = []
+
+const formatDollarsToCents = value => {
+    return value ? Math.round(value * 100) : 0;
+}
+
+const formatCentsToCoins = cents => {
+    let currentCents = cents
+    let coinTypes = [25, 10, 5, 1]
+
+    for (const coin of coinTypes) {
+        centsArray.push(Math.trunc(currentCents / coin))
+        currentCents = cents % coin
+    }
+
+    return centsArray
+}
+
+const displayCoins = coinArray => {
+    // for (const index of coinList) {
+    //     console.log(index)
+    // }
+    for (let i = 0; i < coinList.length; i++) {
+        // console.log(coinList[i])
+        // coinList[i].firstChild.text = coinArray[0]
+        coinList[i].firstChild.textContent = coinArray[i]
+    }
+}
+
+form.addEventListener("submit", event => {
     event.preventDefault()
     console.log("submitted!")
     
-    const dollarAmount = event.target.querySelector("#dollar-input").value
+    const dollarAmount = input.value 
 
     if (isFinite(dollarAmount)) {
-        console.log(`Its a number! ${dollarAmount}`)
+        const totalCents = formatDollarsToCents(dollarAmount)
+        const coins = formatCentsToCoins(totalCents)
+        console.log(`Here are your total cents! ${coins}`)
+        displayCoins(centsArray)
         
     } else {
-        console.log(`Its a string! ${dollarAmount}`)
+        alert(`Please input a correct dollar amount! ${dollarAmount}`)
+        input.value = ''
     }
 
 })
+
+
 
