6a4a468768cde3afc66ca0b4cba07f9f846b9b8b
Spike <jchoi166@gmail.com>
2021-04-13 08:22:59 -0700

    started id search functionality:

--- FetchPractice/script.js
+++ FetchPractice/script.js
@@ -1,7 +1,9 @@
 const displayJoke = document.querySelector('.display__joke')
 const randomButton = document.querySelector('.display__random button')
-let obj
+const idButton = document.querySelector('.display__specific button')
+const idInput = document.querySelector('.display__specific input')
 
+// Random Joke
 const getRandomJoke = () => {
 
     fetch("http://api.icndb.com/jokes/random")
@@ -15,9 +17,33 @@ const getRandomJoke = () => {
     // console.log(joke)
 }
 
-getRandomJoke()
+// getRandomJoke()
 
 randomButton.addEventListener('click', function() {
     console.log('working')
     getRandomJoke()
+})
+
+// Joke with ID 
+
+const getJokeByID = async (id) => {
+    try {
+        const response = await fetch(`http://api.icndb.com/jokes/${id}`)
+        const joke = await response.json()
+        
+        if (joke.type !== "success") throw new Error('A joke with that ID does not exist!');
+
+        console.log(response)
+        console.log(joke.value.joke)
+
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+getJokeByID(134343434)
+
+idButton.addEventListener('click', function() {
+    console.log(idInput.value)
+    getJokeByID(idInput.value)
 })
